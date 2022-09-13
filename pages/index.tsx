68abe2f5b4d30176a60fdc8027b0c8660e4cebf1
Merv <79685609+jdesolate@users.noreply.github.com>
2022-09-14 03:01:40 +0800

    [Feature][Merv] Added Text & Password Field Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,15 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/_home.module.scss";
 import Basic_Header from "../components/Basic_Header/index";
+import Text_Field from "../components/Text_Field";
+import Pass_Field from "../components/Pass_Field";
 
 const Home: NextPage = () => {
     return (
         <MantineProvider>
             <Basic_Header/>
+            <Text_Field label={'Email'} placeholder={'lezzml.now@gmail.com'} />
+            <Pass_Field placeholder={'Your password'} />
         </MantineProvider>
     );
 };
