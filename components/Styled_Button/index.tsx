cdc0b62cc97e05ac62840867d9faf4b64b69a06c
Merv <79685609+jdesolate@users.noreply.github.com>
2022-09-15 03:04:41 +0800

    [Adjustments][Merv] Adjust header and button components

--- components/Styled_Button/index.tsx
+++ components/Styled_Button/index.tsx
@@ -4,14 +4,14 @@ import style from "./_index.module.scss"
 
 type Button_Params = {
   children: React.ReactNode;
-  types?: "sign-in" | "register" | "sign-up" | "banner" | "submit" | "card";
-  themes?: "light" | "dark";
+  types: "sign-in" | "register" | "sign-up" | "banner" | "submit" | "card";
+  theme: "light" | "dark";
 } 
 
 const Styled_Button = (params: Button_Params) => {
-  const {children, types, themes} = params;
+  const {children, types, theme} = params;
   return (
-    <Button className={style[types!]}><h1>{children}</h1></Button>
+    <Button className={`${style[types]} ${style[theme]}`}><h1>{children}</h1></Button>
   )
 }
 
