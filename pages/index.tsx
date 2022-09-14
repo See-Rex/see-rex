d75d34afe3a065eb1ac0ec1710148dd588625b54
Merv Tampus <79685609+jdesolate@users.noreply.github.com>
2022-09-14 21:20:17 +0800

    [Feature][Merv] Add Icon Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,11 @@ import { MantineProvider } from '@mantine/core';
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/_home.module.scss";
-import Basic_Header from "../components/Basic_Header/index";
+import Basic_Header from "../components/Basic_Header";
 import Text_Field from "../components/Text_Field";
 import Pass_Field from "../components/Pass_Field";
+import Icon_Button from "../components/Icon_Button";
+import ResidentialIcon from "../public/residential.svg";
 
 const Home: NextPage = () => {
     return (
@@ -13,6 +15,7 @@ const Home: NextPage = () => {
             <Basic_Header/>
             <Text_Field label={'Email'} placeholder={'lezzml.now@gmail.com'} />
             <Pass_Field placeholder={'Your password'} />
+            <Icon_Button iconSrc={ResidentialIcon} label={"Residential Properties"} />
         </MantineProvider>
     );
 };
