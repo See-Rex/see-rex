f94ab249f6291d91de38f6a778178b4d0b69b444
Merv <79685609+jdesolate@users.noreply.github.com>
2022-09-15 03:04:41 +0800

    [Adjustments][Merv] Adjust header and button components

--- components/BasicHeader/index.tsx
+++ components/BasicHeader/index.tsx
@@ -1,27 +1,37 @@
-import { Header, Group, Container } from '@mantine/core';
-import Icon from '../../public/Icon';
+import { Header, Group, Burger, Container } from '@mantine/core';
+import StyledButton from '../StyledButton';
+import SeeRexIcon from '../../public/Icon';
 import style from "./_index.module.scss";
 
 type Props = {
-  children: React.ReactNode;
-  theme?: "light" | "dark";
-} 
+  type: 'withButtons' | 'desktop' | 'mobile';
+  theme: 'light' | 'dark';
+}
 
 const BasicHeader = (props: Props) => {
-  const {children, theme} = props;
+  const { type, theme } = props;
 
-  return (
-    <Header height={56} mb={120}>
-      <Container>
-        <div className={style.inner}>
-          <Icon/>
-          <Group spacing={20} className={style.links}>
-            {children}
-          </Group>
-        </div>
-      </Container>
-    </Header>
-  );
+  //seerex logo needs to be changed when in dark mode
+  if (type === 'withButtons') {
+    return <Header height={56} mb={20} className={style[theme]}>
+    <Container>
+      <div className={`${style.inner}`}>
+        <SeeRexIcon/>
+        <Group spacing={5} className={style.links}>
+          <StyledButton types='sign-in' theme={theme}>SIGN-IN</StyledButton>
+          <StyledButton types='register' theme={theme}>REGISTER</StyledButton>
+        </Group>
+      </div>
+    </Container>
+  </Header>;
+  } else if (type === 'desktop') {
+    return <Header height={56} mb={20} className={`${style.inner} ${style[theme]}`}> <SeeRexIcon/></Header>;
+  }
+
+  return <Header height={56} mb={20} className={`${style.inner} ${style[theme]}`}>
+      <SeeRexIcon/>
+      <Burger opened={false} color={theme === 'light'? 'black':'white'} />
+    </Header>;
 }
 
 export default BasicHeader;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,27 @@
 import { useState } from "react";
 import type { NextPage } from "next";
 import { MantineProvider, Button } from '@mantine/core';
-import styles from "../styles/_home.module.scss";
 
-import {BasicHeader, TextField, PassField, StyledButton, LandingBanner} from "../components";
+import {BasicHeader, TextField, PassField, LandingBanner} from "../components";
 import Basic_Footer from "../components/Basic_Footer";
 import Icon_Button from "../components/Icon_Button";
 import ResidentialIcon from "../public/residential.svg";
 
+
 const Home: NextPage = () => {
-    const [isFooterLight, setIsFooterLight] = useState(true);
+    const [isLightTheme, setIsLightTheme] = useState(true);
 
     return (
         <MantineProvider>
-            <BasicHeader>
-                <StyledButton type='sign-in'>SIGN-IN</StyledButton>
-                <StyledButton type='register'>REGISTER</StyledButton>
-            </BasicHeader>
             <LandingBanner />
+            <BasicHeader type="withButtons" theme={isLightTheme? 'light':'dark'} />
+            <BasicHeader type="desktop" theme={isLightTheme? 'light':'dark'} />
+            <BasicHeader type="mobile" theme={isLightTheme? 'light':'dark'} />
+            <Button mb={50} onClick={()=>setIsLightTheme(!isLightTheme)}>Change Theme</Button>
+            
             <TextField label={'Email'} placeholder={'lezzml.now@gmail.com'} />
             <PassField placeholder={'Your password'} />
-            <Button onClick={()=>setIsFooterLight(!isFooterLight)} mt={120}>Change Footer Theme</Button>
-            <Basic_Footer type={isFooterLight ? 'light' : 'dark'} />
+            <Basic_Footer type={isLightTheme ? 'light' : 'dark'} />
             <Icon_Button iconSrc={ResidentialIcon} label={"Residential Properties"} />
         </MantineProvider>
     );
