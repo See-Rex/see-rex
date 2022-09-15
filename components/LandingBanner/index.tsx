ea734a2a54a43918e2d3ac9ba2b632470f13347d
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-09-15 10:53:03 +0000

    [Misc][Taos] Finished Hero Banner

--- components/LandingBanner/index.tsx
+++ components/LandingBanner/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import { Title, Text, Container, Button, Overlay, createStyles } from '@mantine/core';
 import style from "./_index.module.scss"
 
 function LandingBanner() {
+
   return (
-    <div className={style.container}>
-      <h1 className={style.header2}>Manage your real estate. <span>Anytime. Anywhere.</span></h1>
-      <h3 className={style.heading}>Explore ways to monitor your properties. Keep in touch with both land and home owners. Create a centralized information database. Integrate data analytics to get better insight of your business.</h3>
+    <div className={style.wrapper}>
+      <Overlay color="#000" opacity={0.65} zIndex={-1} />
+
+      <div className={style.inner}>
+        <Title className={style.title}>
+          Manage your real estate.{' '}
+          <Text component="span" inherit className={style.highlight}>
+            Anytime. Anywhere.
+          </Text>
+        </Title>
+
+        <Container size={640}>
+          <Text size="lg" className={style.description}>
+            Explore ways to monitor your properties. Keep in touch with both land and home owners. Create a centralized information database. Integrate data analytics to get better insight of your business.
+          </Text>
+        </Container>
+
+        <div className={style.controls}>
+          <Button className={style.control} variant="white" size="lg">
+            Get started
+          </Button>
+          <Button className={style.secondaryControl} size="lg">
+            Sign-in
+          </Button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default LandingBanner
