fcd45ad92837281df9fec53e27bdfc6b91dd2283
Jedi Floreta <cyberblazeflo7@gmail.com>
2022-09-18 21:45:33 +0800

    [Feature][Jez] Added skeleton page for dynamic homeowner with props setup
    - Added static path handling with query retrieving homeowners
    - Added static props handler with query to homeowner document list
    - Specified 60 second refresh rate for incremental static regeneration
    - Added interface for props that will contain static data of all homeowners

---
+++ pages/homeowner/[homeowner_slug].tsx
@@ -0,0 +1,69 @@
+import { GetStaticProps } from 'next';
+import React from 'react'
+import { sanityClient } from '../../sanity';
+import { homeowners } from "../../typings"; // Will be resolved once typings file gets merged to main
+
+interface Props {
+  homeowner: Homeowner; // Will be resolved once typings file gets merged to main
+}
+
+function Homeowner({ homeowner }: Props) {
+  return (
+    // `homeowner` is the props (array of returned data in json)
+    // `homeowner`will be used when referencing the data from the CMS 
+    <div><h1>Homeowner</h1></div>
+  )
+}
+
+export default Homeowner
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "homeowner"] | order(dateRegistered desc) {
+    _id,
+    slug {
+      current
+    }
+  }`;
+
+  const homeowners = await sanityClient.fetch(query);
+
+  const owner_paths = homeowners.map((homeowner: Homeowner) => ({
+    params: {
+      slug: homeowner.slug.current,
+    }
+  }));
+
+  return {
+    owner_paths,
+    fallback: "blocking",
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "homeowner"] | order(dateRegistered desc) {
+    _id,
+    name,
+    slug,
+    dateRegistered,
+    contactDetails,
+    bio,
+    mainImage
+  }`;
+
+  const homeowner = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  });
+
+  if (!homeowner) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      homeowner,
+    },
+    revalidate: 60,
+  }
+}
