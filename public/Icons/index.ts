e0996c5eb925b36e7854aeded1c6c72a469914d5
Nash Uriel A. Tapayan <taps.tapayan@symph.co>
2022-09-19 21:05:22 +0800

    [Feature][Taps] Added Customized Icon Pack

---
+++ public/Icons/index.ts
@@ -0,0 +1,12 @@
+import IconContacts from "./IconContacts";
+import IconHolidayVillage from "./IconHolidayVillage";
+import IconLandscape from "./IconLandscape";
+import IconLocationCity from "./IconLocationCity";
+import IconLogin from "./IconLogin";
+export {
+    IconContacts,
+    IconHolidayVillage,
+    IconLandscape,
+    IconLocationCity,
+    IconLogin,
+};
