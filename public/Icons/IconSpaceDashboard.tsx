46109e5b9ce7e73c891100f8e079635ecd0daedf
Nash Uriel A. Tapayan <taps.tapayan@symph.co>
2022-09-19 21:30:49 +0800

    [Misc][Tapayan] Added Missing Icons

--- public/Icons/IconSpaceDashboard.tsx
+++ public/Icons/IconSpaceDashboard.tsx
@@ -5,18 +5,15 @@ import style from "./_index.module.scss";
 
 type Props = { size?: MantineSize };
 
-const IconLogin = (props: Props) => {
+const IconSpaceDashboard = (props: Props) => {
     const { size } = props;
     return (
         <div className={style[size || "md"]}>
             <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
-                <path
-                    className={style.color}
-                    d="M24.45 42v-3H39V9H24.45V6H39q1.2 0 2.1.9.9.9.9 2.1v30q0 1.2-.9 2.1-.9.9-2.1.9Zm-3.9-9.25L18.4 30.6l5.1-5.1H6v-3h17.4l-5.1-5.1 2.15-2.15 8.8 8.8Z"
-                />
+                <path d="M9 42q-1.2 0-2.1-.9Q6 40.2 6 39V9q0-1.2.9-2.1Q7.8 6 9 6h30q1.2 0 2.1.9.9.9.9 2.1v30q0 1.2-.9 2.1-.9.9-2.1.9Zm0-3h13.5V9H9v30Zm16.5 0H39V23.95H25.5Zm0-18.05H39V9H25.5Z" />
             </svg>
         </div>
     );
 };
 
-export default IconLogin;
+export default IconSpaceDashboard;
