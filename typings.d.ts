080ffeac3a06efdd33c1a82be08981db1dc22ba4
Jedi Floreta <cyberblazeflo7@gmail.com>
2022-09-19 16:15:10 +0800

    [Fix][Jez] Fixed Slug Structure and Minor Syntax
    - fixed slug structure to include current state
    - replaced `,` with `;`
    - replaced `.tsx` line ending with `.ts`

--- typings.d.ts
+++ typings.d.ts
@@ -1,58 +1,64 @@
 export interface Homeowner {
-  _id: string,
-  name: string,
-  slug: string,
+  _id: string;
+  name: string;
+  slug: {
+    current: string;
+  };
   image: {
     asset: {
       url: string;
-    }
-  },
-  contactDetails: string,
-  dateRegistered: string,
-  bio: [object]
+    };
+  };
+  contactDetails: string;
+  dateRegistered: string;
+  bio: [object];
 }
 
 export interface Property {
-  _id: string,
-  title: string,
-  slug: string,
+  _id: string;
+  title: string;
+  slug: {
+    current: string;
+  };
   homeowner: {
     name: string;
     image: string;
-  },
+  };
   mainImage: {
     asset: {
       url: string;
-    }
-  },
-  dateRegistered: string,
+    };
+  };
+  dateRegistered: string;
   categories: {
     title: string;
     description: string;
-  },
+  };
   homeownerHistory: {
     name: string;
     image: string;
-  },
+  };
   vehicles: {
     name: string;
     image: string;
     dateRegistered: string;
     proofOfOwnership: [object];
-  },
-  description: [object]
+  };
+  description: [object];
 }
 
 export interface Vehicle {
-  _id: string,
-  name: string,
-  slug: string,
+  _id: string;
+  name: string;
+  slug: {
+    current: string;
+  };
   image: {
     asset: {
       url: string;
-    }
-  },
-  contactDetails: string,
-  dateRegistered: string,
-  bio: [object]
+    };
+  };
+  contactDetails: string;
+  dateRegistered: string;
+  bio: [object];
 }
