e1fb225d086a4700f852f952a3203ecac8c395a7
Merv <79685609+jdesolate@users.noreply.github.com>
2022-09-27 14:39:58 +0800

    [Adjustments][Merv] Make Auth Layouts Adaptive

--- layouts/AuthLayout/index.tsx
+++ layouts/AuthLayout/index.tsx
@@ -1,121 +1,49 @@
-import { 
-  Anchor,
-  Group,
-  Container, 
-  Paper, 
-  Text,
-  Divider,
-  Title
-} from '@mantine/core';
-
-import {
-  BasicHeader,
-  BasicFooter,
-  GoogleButton,
-  IconButton,
-  InputField,
-  StyledButton
-} from "../../components";
-import { useToggle } from "@mantine/hooks";
-
+import { Paper, Group, Container } from "@mantine/core";
 import style from "./_index.module.scss";
-import SeeRexIcon from '../../public/Logo';
-import { useState } from 'react';
-import ArrowBack from '../../public/Icons/ArrowBack';
+import {
+    BasicHeader,
+    BasicFooter,
+} from "../../components";
+import SeeRexIcon from "../../public/Logo";
+import { useAuth } from "../../context/AuthContext";
+import { useRouter } from "next/router";
+import React from "react";
 
-export type AuthType = {
-    type: 'login' | 'sign-up' | 'reset';
+type Props = {
+    children: React.ReactNode;
+    isForgotPassword?: boolean;
 }
 
-const AuthLayout = (props: AuthType) => {
-  const [type, toggle] = useToggle(['login', 'register']);
-  const [isForgotPass, setIsForgotPass] = useState(false);
+export function AuthLayout(props: Props) {
+    const { children, isForgotPassword } = props;
+    const { user } = useAuth();
+    const router = useRouter();
 
-  const renderForgotPasswordTitle = isForgotPass && <Group position='center'> 
-    <Title size={20} align="center" className={style.forgotPassTitle}>
-      Forgot your password?
-    </Title>
-    <Text color="dimmed" size="sm" align="center" mt={-10}>
-      Enter your email address to get a reset password link.
-    </Text>
-  </Group>
+    if (user) {
+        router.push("/dashboard");
+    }
 
-  const renderForgotPasswordAnchor = type === 'login' && <Group position="right" mt="md">
-    <Anchor<'a'> 
-      onClick={() => setIsForgotPass(true)}
-      size="sm"
-      className={style.forgotPass}
-      type="button"
-    >
-      Forgot password?
-    </Anchor>
-  </Group>;
-
-  const renderForgotPasswordForm = isForgotPass && <Group position="apart" mt="md">
-    <IconButton 
-      className={'back'}
-      icon={<ArrowBack />}
-      isFullWidth={false}
-      label={'Back to login page'} 
-      onClick={() => setIsForgotPass(false)}
-    />
-    <StyledButton types='submit'>RESET PASSWORD</StyledButton>
-  </Group>;
-
-  const renderSignInButtons =  type === 'login' && <Group position='center'>
-    <StyledButton types='sign-in'>SIGN-IN</StyledButton>
-    <StyledButton types='register' onClick={() => toggle()}>REGISTER</StyledButton>
-  </Group>;
-  
-  const renderSignUpButton =  type === 'register' && 
-    <StyledButton types='sign-up' onClick={() => toggle()}>SIGN-UP</StyledButton>;
-  
-  const renderConfirmPasswordField =  type === 'register' && 
-    <InputField label={'Confirm Password'} placeholder={'Re-enter password'} />;
-
-  const renderDefaultView = !isForgotPass && <>
-    <InputField label={'Password'} placeholder={'Your password'} />
-    {renderConfirmPasswordField}
-      <Group position='center' mb="md" mt="md">
-        {renderSignInButtons}
-        {renderSignUpButton}
-      </Group>
-      {renderForgotPasswordAnchor}
-      <Divider label="OR" labelPosition="center" my="lg" />
-      <Group grow mb="md" mt="md">
-        <GoogleButton radius="sm" className={style.google}>Continue with Google</GoogleButton>
-      </Group>
-      <Group position='center'>
-        <Text className={style.text}>{type === 'register' && "Already have an account?"}</Text>
-        <Anchor<'a'> 
-            onClick={() => toggle()}
-            href="#" 
-            size="sm"
-            className={style.forgotPass}
-            type="button"
-          >
-            {type === 'register' && "Login"}
-        </Anchor>
-      </Group>
-  </>
-
-  return (
-    <Paper className={style.container}>
-      <BasicHeader opened={false} />
-      <Container my={50}>
-        <Paper withBorder shadow="md" p={35} mt={20} mb={30} radius="md" className={style.formContainer}>
-          <Group position="center" mt="md" mb="xl">
-            <SeeRexIcon />
-          </Group>
-          {renderForgotPasswordTitle}
-          <InputField label={'Email'} placeholder={'lezzml.now@gmail.com'} />
-          {renderDefaultView}
-          {renderForgotPasswordForm}
-        </Paper>
-      </Container>
-      <BasicFooter height={56} />
-    </Paper>
-  );
+    return (
+        <>
+            <BasicHeader opened={false} />
+            <Container>
+                <Paper
+                    withBorder
+                    shadow="md"
+                    radius="md"
+                    className={ isForgotPassword ? style.forgotContainer : style.formContainer}
+                    p='xl'
+                    m='xl'
+                >
+                        <Group position="center" my='sm'>
+                            <SeeRexIcon />
+                        </Group>
+                        {children}
+                </Paper>
+            </Container>
+            <BasicFooter height={56} />
+        </>
+    );
 }
 
 export default AuthLayout;
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,21 +1,17 @@
 import { useForm } from "@mantine/form";
-import { Paper, Group, Divider, Anchor, Container } from "@mantine/core";
+import { Group, Divider, Anchor, Stack } from "@mantine/core";
 import style from "./_index.module.scss";
 import {
-    BasicHeader,
     InputField,
     StyledButton,
     GoogleButton,
-    BasicFooter,
 } from "../../components";
-import SeeRexIcon from "../../public/Logo";
 import Link from "next/link";
 import { useAuth } from "../../context/AuthContext";
-import { useRouter } from "next/router";
+import { AuthLayout } from "../../layouts/AuthLayout";
 
 export function Login() {
-    const { user, login } = useAuth();
-    const router = useRouter();
+    const { login } = useAuth();
     const form = useForm({
         initialValues: {
             email: "",
@@ -35,96 +31,76 @@ export function Login() {
         try {
             await login(form.values.email, form.values.password);
             alert("You have successfully logged in!");
-            router.push("/dashboard");
         } catch (err) {
             alert(err);
         }
     };
 
-    if (user) {
-        router.push("/dashboard");
-    }
-
     return (
-        <Paper className={style.container}>
-            <BasicHeader opened={false} />
-            <Container my={50}>
-                <Paper
-                    withBorder
-                    shadow="md"
-                    p={35}
-                    mt={20}
-                    mb={30}
-                    radius="md"
-                    className={style.formContainer}
-                >
-                    <Group position="center" mt="md" mb="xl">
-                        <SeeRexIcon />
-                    </Group>
-                    <form onSubmit={form.onSubmit(handleLogin)}>
-                        <InputField
-                            required
-                            label={"Email"}
-                            placeholder={"lezzml.now@gmail.com"}
-                            error={form.errors.email && "Invalid email"}
-                            value={form.values.email}
-                            onChange={(event) =>
-                                form.setFieldValue(
-                                    "email",
-                                    event.currentTarget.value,
-                                )
-                            }
-                        />
-                        <InputField
-                            required
-                            label={"Password"}
-                            placeholder={"Your password"}
-                            value={form.values.password}
-                            onChange={(event) =>
-                                form.setFieldValue(
-                                    "password",
-                                    event.currentTarget.value,
-                                )
-                            }
-                            error={
-                                form.errors.password &&
-                                "Password should include at least 6 characters"
-                            }
-                            type="password"
-                        />
-                        <Group position="center" mb="md" mt="md">
-                            <Group position="center">
-                                <StyledButton types="sign-in">
-                                    SIGN-IN
+        <AuthLayout>
+            <form onSubmit={form.onSubmit(handleLogin)}>
+                <Stack spacing='md'>
+                    <InputField
+                        required
+                        label={"Email"}
+                        placeholder={"lezzml.now@gmail.com"}
+                        error={form.errors.email && "Invalid email"}
+                        value={form.values.email}
+                        onChange={(event) =>
+                            form.setFieldValue(
+                                "email",
+                                event.currentTarget.value,
+                            )
+                        }
+                    />
+                    <InputField
+                        required
+                        label={"Password"}
+                        placeholder={"Your password"}
+                        value={form.values.password}
+                        onChange={(event) =>
+                            form.setFieldValue(
+                                "password",
+                                event.currentTarget.value,
+                            )
+                        }
+                        error={
+                            form.errors.password &&
+                            "Password should include at least 6 characters"
+                        }
+                        type="password"
+                    />
+                    <Group position="center">
+                        <Group position="center">
+                            <StyledButton types="sign-in">
+                                SIGN-IN
+                            </StyledButton>
+                            <Link href="/auth/register">
+                                <StyledButton types="register">
+                                    REGISTER
                                 </StyledButton>
-                                <Link href="/auth/register">
-                                    <StyledButton types="register">
-                                        REGISTER
-                                    </StyledButton>
-                                </Link>
-                            </Group>
+                            </Link>
                         </Group>
-                        <Group position="right" mt="md">
-                            <Anchor<"a">
-                                href="/auth/resetpassword"
-                                size="sm"
-                                className={style.forgotPass}
-                                type="button"
-                            >
-                                Forgot password?
-                            </Anchor>
-                        </Group>
-                        <Divider label="OR" labelPosition="center" my="lg" />
-                        <Group grow mb="md" mt="md">
-                            <GoogleButton radius="sm" className={style.google}>
-                                Continue with Google
-                            </GoogleButton>
-                        </Group>
-                    </form>
-                </Paper>
-            </Container>
-            <BasicFooter height={56} />
-        </Paper>
+                    </Group>
+                    <Group position="right">
+                        <Anchor<"a">
+                            href="/auth/resetpassword"
+                            size="sm"
+                            className={style.forgotPass}
+                            type="button"
+                        >
+                            Forgot password?
+                        </Anchor>
+                    </Group>
+                    <Divider label="OR" labelPosition="center" />
+                    <Group grow>
+                        <GoogleButton radius="sm" className={style.google}>
+                            Continue with Google
+                        </GoogleButton>
+                    </Group>
+                </Stack>
+            </form>
+        </AuthLayout>
     );
 }
 
