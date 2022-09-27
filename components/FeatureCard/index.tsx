9cb596afd8aba053824dc30bc8e70fbcc14be229
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-09-27 15:49:57 +0800

    [Feature][Tapayan] Feature Card Creation
    - Adding of FeatureCard Component

---
+++ components/FeatureCard/index.tsx
@@ -0,0 +1,38 @@
+import { Group, Stack, Text, ThemeIcon } from "@mantine/core";
+import React from "react";
+
+import style from "./_index.module.scss";
+
+type Props = {
+  description: string;
+  icon: React.ReactNode;
+  title: string;
+};
+
+function FeatureCard(props: Props) {
+  const { description, icon, title } = props;
+  return (
+    <div key={title}>
+      <ThemeIcon
+        size={60}
+        radius="md"
+        variant="gradient"
+        gradient={{ deg: 133, from: "#2F90B0", to: "#2F90B0" }}
+      >
+        {icon}
+      </ThemeIcon>
+      <Stack>
+        <Group position="apart">
+          <Text size="lg" mt="sm" className={style.title}>
+            {title}
+          </Text>
+        </Group>
+        <Text color="dimmed" className={style.description}>
+          {description}
+        </Text>
+      </Stack>
+    </div>
+  );
+}
+
+export default FeatureCard;
