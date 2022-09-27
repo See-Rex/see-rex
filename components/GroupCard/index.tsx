4ba9c005268e69c26f82db4c97823e84043f68ca
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-09-27 12:44:38 +0800

    [Feature][Tapayan] Adding of Group Statistic Card

---
+++ components/GroupCard/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text } from "@mantine/core";
+import style from "./_index.module.scss";
+
+interface StatsGroupProps {
+  data: { title: string; stats: string; description: string }[];
+}
+
+function GroupCard({ data }: StatsGroupProps) {
+  const stats = data.map((stat) => (
+    <div key={stat.title} className={style.stat}>
+      <Text className={style.count}>{stat.stats}</Text>
+      <Text className={style.title}>{stat.title}</Text>
+      <Text className={style.description}>{stat.description}</Text>
+    </div>
+  ));
+  return <div className={style.root}>{stats}</div>;
+}
+
+export default GroupCard;
