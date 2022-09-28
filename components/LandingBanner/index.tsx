5aa7802ffe46fc33e10a25429b2c16b7d29ead3d
Nash Uriel A. Tapayan <taps.tapayan@symph.co>
2022-09-28 20:00:28 +0800

    [Adjustments][Tapayan] Fixed Component Importation

--- components/LandingBanner/index.tsx
+++ components/LandingBanner/index.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import { Title, Text, Container, Button, Overlay, createStyles } from '@mantine/core';
-import style from "./_index.module.scss"
+import React from "react";
+import { Title, Text, Container, Button, Overlay } from "@mantine/core";
+import style from "./_index.module.scss";
 
 function LandingBanner() {
-
   return (
     <div className={style.wrapper}>
       <Overlay color="#000" opacity={0.65} zIndex={-1} />
 
       <div className={style.inner}>
         <Title className={style.title}>
-          Manage your real estate.{' '}
+          Manage your real estate.{" "}
           <Text component="span" inherit className={style.highlight}>
             Anytime. Anywhere.
           </Text>
@@ -18,7 +17,9 @@ function LandingBanner() {
 
         <Container size={640}>
           <Text size="lg" className={style.description}>
-            Explore ways to monitor your properties. Keep in touch with both land and home owners. Create a centralized information database. Integrate data analytics to get better insight of your business.
+            Explore ways to monitor your properties. Keep in touch with both
+            land and home owners. Create a centralized information database.
+            Integrate data analytics to get better insight of your business.
           </Text>
         </Container>
 
@@ -35,4 +36,4 @@ function LandingBanner() {
   );
 }
 
-export default LandingBanner
+export default LandingBanner;
--- components/StyledButton/index.tsx
+++ components/StyledButton/index.tsx
@@ -4,26 +4,30 @@ import style from "./_index.module.scss";
 import { LinkProps } from "next/link";
 
 type Button_Params = {
-    children: React.ReactNode;
-    types:
-        | "sign-in"
-        | "register"
-        | "sign-up"
-        | "banner"
-        | "submit"
-        | "card"
-        | "navigation";
+  children: React.ReactNode;
+  types:
+    | "sign-in"
+    | "register"
+    | "sign-up"
+    | "banner"
+    | "submit"
+    | "card"
+    | "navigation";
 };
 
 const StyledButton = (params: Omit<LinkProps, "href"> & Button_Params) => {
-    const { colorScheme } = useMantineColorScheme();
-    const { children, onClick, types } = params;
-    
-    return (
-        <Button className={`${style[types]} ${style[colorScheme]}`} onClick={onClick} type="submit">
-            <h1>{children}</h1>
-        </Button>
-    );
+  const { colorScheme } = useMantineColorScheme();
+  const { children, onClick, types } = params;
+
+  return (
+    <Button
+      className={`${style[types]} ${style[colorScheme]}`}
+      onClick={onClick}
+      type="submit"
+    >
+      <h1>{children}</h1>
+    </Button>
+  );
 };
 
 export default StyledButton;
