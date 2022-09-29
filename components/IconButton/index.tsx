7be5b553131890f61cf5288ba25d9c1594fdb4ba
Nash Uriel A. Tapayan <taps.tapayan@symph.co>
2022-09-30 01:13:03 +0800

    [Adjustments][Tapayan] Styling Fixes in IconButton Component

--- components/IconButton/index.tsx
+++ components/IconButton/index.tsx
@@ -5,34 +5,27 @@ import React from "react";
 import style from "./_index.module.scss";
 
 type Props = {
-    icon: React.ReactNode;
-    label: string;
-    className?: "link" | "active" | "back";
-    isFullWidth?: boolean;
+  icon: React.ReactNode;
+  label: string;
+  className?: "link" | "active" | "back";
+  isFullWidth?: boolean;
 };
 
 const IconButton = (props: Omit<LinkProps, "href"> & Props) => {
-    const { colorScheme } = useMantineColorScheme();
-    const { icon, label, className, onClick, isFullWidth } = props;
-    const isIconArrowBack = className?.includes('back');
-
-    const classWithColorScheme = style[colorScheme + '-' + className ?? 'link'];
-    const isIconNavButton = !isIconArrowBack && style.iconButton;
-    
-    const renderButtonContent = isIconArrowBack? label : <h1>{label}</h1>;
-
-    return (
-        <Button
-            className={`${isIconNavButton} ${classWithColorScheme} ${isIconArrowBack && style.back}`}
-            variant="subtle"
-            leftIcon={icon}
-            fullWidth={isFullWidth}
-            size="sm"
-            onClick={onClick}
-        >
-            {renderButtonContent}
-        </Button>
-    );
+  const { colorScheme } = useMantineColorScheme();
+  const { icon, label, className, onClick, isFullWidth } = props;
+  return (
+    <Button
+      className={`${style[colorScheme]} ${style[className || "link"]}`}
+      variant="subtle"
+      leftIcon={icon}
+      fullWidth={isFullWidth}
+      size="sm"
+      onClick={onClick}
+    >
+      <h1>{label}</h1>
+    </Button>
+  );
 };
 
 export default IconButton;
