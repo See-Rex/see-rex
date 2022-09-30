bf8d2e339bec8a99240359dec9533a4cc5768e99
Nash Uriel A. Tapayan <taps.tapayan@symph.co>
2022-09-30 17:52:34 +0800

    [Feature][Tapayan] Added Story Page
    - Added Story Page Routing
    - Relocated Dev Handout
    - Added Story Page Styling

---
+++ components/StoryBar/index.tsx
@@ -0,0 +1,170 @@
+import {
+  Button,
+  Navbar,
+  NavbarProps,
+  ScrollArea,
+  useMantineColorScheme,
+} from "@mantine/core";
+import style from "./_index.module.scss";
+import React from "react";
+import IconButton from "../IconButton";
+import { IconLogout } from "./../../public/Icons";
+import { useRouter } from "next/router";
+import { StyledButton } from "..";
+
+type Props = {
+  page: number;
+  setPage: Function;
+};
+
+function StoryBar(props: Omit<NavbarProps, "children"> & Props) {
+  const router = useRouter();
+  const { colorScheme } = useMantineColorScheme();
+
+  const { hidden, page, setPage } = props;
+
+  return (
+    <Navbar
+      hidden={hidden}
+      width={{ sm: 350 }}
+      p="md"
+      className={style[colorScheme]}
+    >
+      <Navbar.Section component={ScrollArea} grow>
+        <StyledButton
+          spacing="xs"
+          types={page == 1 ? "active" : "navigation"}
+          onClick={() => setPage(1)}
+          fullWidth
+        >
+          AppCard
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 2 ? "active" : "navigation"}
+          onClick={() => setPage(2)}
+          fullWidth
+        >
+          Basic Footer
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 3 ? "active" : "navigation"}
+          onClick={() => setPage(3)}
+          fullWidth
+        >
+          Basic Header
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 4 ? "active" : "navigation"}
+          onClick={() => setPage(4)}
+          fullWidth
+        >
+          Collapsable Bar
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 5 ? "active" : "navigation"}
+          onClick={() => setPage(5)}
+          fullWidth
+        >
+          Feature Card
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 6 ? "active" : "navigation"}
+          onClick={() => setPage(6)}
+          fullWidth
+        >
+          Feature Hero
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 7 ? "active" : "navigation"}
+          onClick={() => setPage(7)}
+          fullWidth
+        >
+          Google Button
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 8 ? "active" : "navigation"}
+          onClick={() => setPage(8)}
+          fullWidth
+        >
+          Group Card
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 9 ? "active" : "navigation"}
+          onClick={() => setPage(9)}
+          fullWidth
+        >
+          Icon Button
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 10 ? "active" : "navigation"}
+          onClick={() => setPage(10)}
+          fullWidth
+        >
+          Input Field
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 11 ? "active" : "navigation"}
+          onClick={() => setPage(11)}
+          fullWidth
+        >
+          Landing Banner
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 12 ? "active" : "navigation"}
+          onClick={() => setPage(12)}
+          fullWidth
+        >
+          Main Footer
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 13 ? "active" : "navigation"}
+          onClick={() => setPage(13)}
+          fullWidth
+        >
+          Progress Bar
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 14 ? "active" : "navigation"}
+          onClick={() => setPage(14)}
+          fullWidth
+        >
+          Statistic Card
+        </StyledButton>
+        <StyledButton
+          spacing="xs"
+          types={page == 15 ? "active" : "navigation"}
+          onClick={() => setPage(15)}
+          fullWidth
+        >
+          Styled Button
+        </StyledButton>
+      </Navbar.Section>
+      <Navbar.Section className={style.footer}>
+        <IconButton
+          className="link"
+          icon={<IconLogout />}
+          label={"Dev Handout"}
+          onClick={() => {
+            router.push("/dev");
+          }}
+          isFullWidth
+        />
+      </Navbar.Section>
+    </Navbar>
+  );
+}
+
+export default StoryBar;
---
+++ pages/story/index.tsx
@@ -0,0 +1,211 @@
+import { Center, Group, Image, Paper } from "@mantine/core";
+import { IconSearch } from "@tabler/icons";
+import React, { useState } from "react";
+import {
+  AppCard,
+  BasicFooter,
+  BasicHeader,
+  CollapsedBar,
+  FeatureCard,
+  FeatureHero,
+  GoogleButton,
+  GroupCard,
+  IconButton,
+  InputField,
+  LandingBanner,
+  MainFooter,
+  ProgressCard,
+  StatisticCard,
+  StyledButton,
+} from "../../components";
+import { IconHolidayVillage } from "../../public/Icons";
+import StoryLayout from "./../../layouts/StoryLayout";
+
+function StoryPage() {
+  const [opened, setOpened] = useState(false);
+  const [activePage, setActivePage] = useState(1);
+
+  const handlePage = (page: number) => {
+    setActivePage(page);
+  };
+
+  const data = [
+    {
+      title: "Page views",
+      stats: "456,133",
+      description:
+        "24% more than in the same month last year, 33% more that two years ago",
+    },
+    {
+      title: "New users",
+      stats: "2,175",
+      description:
+        "13% less compared to last month, new user engagement up by 6%",
+    },
+    {
+      title: "Completed orders",
+      stats: "1,994",
+      description:
+        "1994 orders were completed this month, 97% satisfaction rate",
+    },
+  ];
+
+  const PageContent = () => {
+    switch (activePage) {
+      case 1:
+        return (
+          <AppCard
+            image={
+              <Image src={"../background2.png"} alt={"homePic"} height={180} />
+            }
+            title={"Home Property #1"}
+            type={"Residential"}
+            description={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            }
+            values={["100", "200", "300", "400"]}
+          />
+        );
+      case 2:
+        return (
+          <>
+            <BasicFooter height={56} />
+            <h1>See Footer Below</h1>
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <BasicHeader
+              opened={opened}
+              onClick={() => setOpened(!opened)}
+              burger
+            />
+            <h1>See The Header Above</h1>
+          </>
+        );
+      case 4:
+        return (
+          <>
+            <CollapsedBar
+              hidden={!opened}
+              page={activePage}
+              setPage={handlePage}
+            />
+            <h1>See The Collapsable Bar Beside</h1>
+          </>
+        );
+      case 5:
+        return (
+          <Paper style={{ background: "#b1b1b1b7", maxWidth: 350 }} p="md">
+            <FeatureCard
+              description="Access all your property’s information within a single website. Search through hundreds of data. Filter just what you need. "
+              icon={<IconSearch />}
+              title="Centralized Database"
+            />
+          </Paper>
+        );
+      case 6:
+        return <FeatureHero />;
+      case 7:
+        return <GoogleButton>Sign in with Google</GoogleButton>;
+      case 8:
+        return <GroupCard data={data} />;
+      case 9:
+        return (
+          <Group position="apart">
+            <>
+              <IconButton
+                className="link"
+                icon={<IconHolidayVillage />}
+                label={"Link Inactive"}
+                isFullWidth
+              />
+            </>
+            <>
+              <IconButton
+                className="active"
+                icon={<IconHolidayVillage />}
+                label={"Link Active"}
+                isFullWidth
+              />
+            </>
+          </Group>
+        );
+      case 10:
+        return (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <InputField label={"Email"} placeholder={"lezzml.now@gmail.com"} />
+            <InputField
+              label={"Password"}
+              placeholder={"Your password"}
+              type="password"
+            />
+          </div>
+        );
+      case 11:
+        return (
+          <div style={{ width: "100%" }}>
+            <LandingBanner />
+          </div>
+        );
+      case 12:
+        return <MainFooter height={56} />;
+      case 13:
+        return (
+          <ProgressCard
+            label="Test"
+            stats="3,450"
+            progress={3450}
+            color="#35C0ED"
+            icon="up"
+          />
+        );
+      case 14:
+        return (
+          <StatisticCard
+            amount={100}
+            category="Testing"
+            description="This how your statistics are shown"
+          />
+        );
+      case 15:
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              rowGap: 20,
+              minWidth: "50%",
+            }}
+          >
+            <StyledButton types="sign-in">Sign-in</StyledButton>
+            <StyledButton types="register">Register</StyledButton>
+            <StyledButton types="navigation">Navigation</StyledButton>
+            <StyledButton types="sign-up">Sign-up</StyledButton>
+            <StyledButton types="banner">Banner</StyledButton>
+            <StyledButton types="card">Card</StyledButton>
+            <StyledButton types="active">Active</StyledButton>
+          </div>
+        );
+      default:
+        return <h1>Error 404: Component not found</h1>;
+    }
+  };
+
+  return (
+    <StoryLayout
+      opened={opened}
+      onClick={() => setOpened((o) => !o)}
+      activePage={activePage}
+      paginator={handlePage}
+    >
+      <Center style={{ height: "100%", width: "100%" }}>
+        <PageContent />
+      </Center>
+    </StoryLayout>
+  );
+}
+
+export default StoryPage;
