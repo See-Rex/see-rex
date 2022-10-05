558371313eb1515ca10a3a8a433886313019ca6f
Merv <79685609+jdesolate@users.noreply.github.com>
2022-10-05 23:08:58 +0800

    [Misc][Merv] Resolve comment

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,47 +1,10 @@
-import { AppShell, Button, Container, createStyles, Group, Text, Title } from '@mantine/core';
+import { AppShell, Button, Container, Group, Text, Title, useMantineColorScheme } from '@mantine/core';
 import { useRouter } from 'next/router';
 import { BasicFooter, BasicHeader } from '../components';
-
-const useStyles = createStyles((theme) => ({
-  description: {
-    margin: 'auto',
-    marginBottom: theme.spacing.xl * 1.5,
-    marginTop: theme.spacing.xl,
-    maxWidth: 500,
-  },
-
-  label: {
-    [theme.fn.smallerThan('sm')]: {
-      fontSize: 120,
-    },
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2],
-    fontSize: 220,
-    fontWeight: 900,
-    lineHeight: 1,
-    marginBottom: theme.spacing.xl * 1.5,
-
-    textAlign: 'center',
-  },
-
-  root: {
-    paddingBottom: 80,
-    paddingTop: 80,
-  },
-
-  title: {
-    [theme.fn.smallerThan('sm')]: {
-      fontSize: 32,
-    },
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontSize: 38,
-    fontWeight: 900,
-
-    textAlign: 'center',
-  },
-}));
+import style from "./_index.module.scss";
 
 function ErrorPage() {
-  const { classes } = useStyles();
+  const { colorScheme } = useMantineColorScheme();
   const router = useRouter();
 
   function handleNavigate() {
@@ -50,13 +13,14 @@ function ErrorPage() {
 
   return (
     <AppShell
-            header={<BasicHeader opened={false} burger />}
-            footer={<BasicFooter height={56} />}
+          className={`${style.shell} ${style[colorScheme]}`}
+          header={<BasicHeader opened={false} burger />}
+          footer={<BasicFooter height={56} />}
         >
-          <Container className={classes.root}>
-            <div className={classes.label}>404</div>
-            <Title className={classes.title}>You have found a secret place.</Title>
-            <Text color="dimmed" size="lg" align="center" className={classes.description}>
+          <Container className={`${style.root} ${style[colorScheme]}`}>
+            <div className={`${style.label} ${style[colorScheme]}`}>404</div>
+            <Title className={`${style.title} ${style[colorScheme]}`}>You have found a secret place.</Title>
+            <Text color="dimmed" size="lg" align="center" className={style.description}>
               Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has
               been moved to another URL.
             </Text>
