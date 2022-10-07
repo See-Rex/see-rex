0530dc656ad82e3aafc9602f74b9f2bf4341fbf3
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-10-07 18:56:15 +0800

    [Feature][Tapayan] Landing Page

--- components/LandingBanner/index.tsx
+++ components/LandingBanner/index.tsx
@@ -1,6 +1,7 @@
-import { Button, Container, Overlay, Text, Title } from "@mantine/core";
-import React from "react";
-import style from "./_index.module.scss";
+import { Button, Container, Overlay, Text, Title } from '@mantine/core';
+import Link from 'next/link';
+import React from 'react';
+import style from './_index.module.scss';
 
 function LandingBanner() {
   return (
@@ -9,7 +10,7 @@ function LandingBanner() {
 
       <div className={style.inner}>
         <Title className={style.title}>
-          Manage your real estate.{" "}
+          Manage your real estate.{' '}
           <Text component="span" inherit className={style.highlight}>
             Anytime. Anywhere.
           </Text>
@@ -17,19 +18,22 @@ function LandingBanner() {
 
         <Container size={640}>
           <Text size="lg" className={style.description}>
-            Explore ways to monitor your properties. Keep in touch with both
-            land and home owners. Create a centralized information database.
-            Integrate data analytics to get better insight of your business.
+            Explore ways to monitor your properties. Keep in touch with both land and home owners. Create a centralized
+            information database. Integrate data analytics to get better insight of your business.
           </Text>
         </Container>
 
         <div className={style.controls}>
-          <Button className={style.control} variant="white" size="lg">
-            Get started
-          </Button>
-          <Button className={style.secondaryControl} size="lg">
-            Sign-in
-          </Button>
+          <Link href="/auth/register">
+            <Button className={style.control} variant="white" size="lg">
+              Get started
+            </Button>
+          </Link>
+          <Link href="/auth/login">
+            <Button className={style.secondaryControl} size="lg">
+              Sign-in
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
