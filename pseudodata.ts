c5666e5db57aa25ecbe48280a267db7055de5542
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-10-11 00:04:14 +0800

    [Adjustments][Tapayan] Changed Table Features

--- pseudodata.ts
+++ pseudodata.ts
@@ -1,5 +1,6 @@
 export const contacts_data = [
   {
+    address: '844 Morris Park avenue',
     avatar:
       'https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     email: 'rob_wolf@gmail.com',
@@ -8,6 +9,7 @@ export const contacts_data = [
     type: 'Land',
   },
   {
+    address: '254 Rizal Park avenue',
     avatar:
       'https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     email: 'jj@breaker.com',
@@ -16,6 +18,7 @@ export const contacts_data = [
     type: 'Commercial',
   },
   {
+    address: '254 Rizal Park avenue',
     avatar:
       'https://images.unsplash.com/photo-1632922267756-9b71242b1592?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     email: 'henry@silkeater.io',
@@ -24,6 +27,7 @@ export const contacts_data = [
     type: 'Commercial',
   },
   {
+    address: '844 Morris Park avenue',
     avatar:
       'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     email: 'bhorsefighter@gmail.com',
@@ -32,6 +36,7 @@ export const contacts_data = [
     type: 'Residential',
   },
   {
+    address: '844 Morris Park avenue',
     avatar:
       'https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     email: 'jeremy@foot.dev',
