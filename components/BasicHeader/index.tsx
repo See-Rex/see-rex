ca14d08698dd41d904bef7e304701f827a4fa070
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-10-11 01:10:05 +0800

    [Adjustments][Tapayan] Minor Header Changes

--- components/BasicHeader/index.tsx
+++ components/BasicHeader/index.tsx
@@ -14,7 +14,7 @@ const BasicHeader = (props: BurgerProps & Props) => {
 
   return (
     <Header height={56} mb={50} className={style[colorScheme]}>
-      <Container fluid={!children} px={children ? 0 : 'lg'}>
+      <Container fluid px="lg">
         <div className={style.inner}>
           <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
             <Burger
