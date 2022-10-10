26856749ffe1b301856e58a2d75406bfc301f487
Merv <79685609+jdesolate@users.noreply.github.com>
2022-10-10 21:58:01 +0800

    [Misc][Merv] Resolve filter styles migration to sass

--- components/FilterPicker/index.tsx
+++ components/FilterPicker/index.tsx
@@ -1,49 +1,12 @@
-import { createStyles, Group, Menu, UnstyledButton, useMantineColorScheme } from '@mantine/core';
+import { Group, Menu, UnstyledButton, useMantineColorScheme } from '@mantine/core';
 import { IconCar, IconChevronDown, IconHomeDollar, IconRulerMeasure, IconTextSize, IconUsers } from '@tabler/icons';
 import { useState } from 'react';
 import PropertyInfo from '../../enums/PropertyInfo.enum';
 import { usePropertyContext } from '../../hooks/PropertyContext';
 import style from "./_index.module.scss";
 
-
-const useStyles = createStyles((theme, { opened }: { opened: boolean }) => ({
-  control: {
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
-    },
-    alignItems: 'center',
-    backgroundColor:
-      theme.colorScheme === 'dark'
-        ? theme.colors.dark[opened ? 5 : 6]
-        : opened
-        ? theme.colors.gray[0]
-        : theme.white,
-    border: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[2]
-    }`,
-    borderRadius: theme.radius.md,
-    display: 'flex',
-    justifyContent: 'space-between',
-    padding: '10px 15px',
-    transition: 'background-color 150ms ease',
-
-    width: 200,
-  },
-
-  icon: {
-    transform: opened ? 'rotate(180deg)' : 'rotate(0deg)',
-    transition: 'transform 150ms ease',
-  },
-
-  label: {
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-  },
-}));
-
 function FilterPicker() {
   const [opened, setOpened] = useState(false);
-  const { classes } = useStyles({ opened });
   const { colorScheme } = useMantineColorScheme();
   const { setPropertyInfoFilterType } = usePropertyContext();
 
@@ -75,6 +38,7 @@ function FilterPicker() {
 
   const items = filterTypeData.map((item) => (
     <Menu.Item
+      className={`${style.dropDown} ${style[colorScheme]}`}
       icon={item.image}
       onClick={() => setSelected(item)}
       key={item.label}
@@ -91,15 +55,15 @@ function FilterPicker() {
       width="target"
     >
       <Menu.Target>
-        <UnstyledButton className={classes.control}>
+        <UnstyledButton className={`${style.control} ${style[colorScheme]}`}>
           <Group spacing="xs">
             {selected.image}
-            <span className={classes.label}>{selected.label}</span>
+            <span className={style.label}>{selected.label}</span>
           </Group>
-          <IconChevronDown size={18} className={classes.icon} stroke={2} />
+          <IconChevronDown size={18} className={opened ? style.iconUp : style.iconDown} stroke={2} />
         </UnstyledButton>
       </Menu.Target>
-      <Menu.Dropdown>{items}</Menu.Dropdown>
+      <Menu.Dropdown className={`${style.dropDown} ${style[colorScheme]}`}>{items}</Menu.Dropdown>
     </Menu>
   );
 }
