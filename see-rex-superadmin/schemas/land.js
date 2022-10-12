bc4447d4d6d2c383ffe86baeb20acfeb38b13bc2
Jedi Floreta <cyberblazeflo7@gmail.com>
2022-10-12 21:16:23 +0800

    [Feature][Floreta] Added land type document

---
+++ see-rex-superadmin/schemas/land.js
@@ -0,0 +1,49 @@
+export default {
+  name: 'landProperty',
+  title: 'Land Property',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'dateRegistered',
+      title: 'Date Registered',
+      type: 'date',
+      options: {
+        dateFormat: 'YYYY-MM-DD',
+        calendarTodayLabel: 'Today',
+      },
+    },
+    {
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'category' } }],
+    },
+    {
+      name: 'description',
+      title: 'Description / Defining Features',
+      type: 'blockContent',
+    },
+  ],
+};
