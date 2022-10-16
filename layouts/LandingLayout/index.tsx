4f97f43b3de91aace298c47d0620df40064e3e5e
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-10-16 17:16:35 +0800

    Revert "[Misc][Tapayan] Landing Page Changes"
    This reverts commit 2062384ec6718370248a4764ebca8361356236f4.

--- layouts/LandingLayout/index.tsx
+++ layouts/LandingLayout/index.tsx
@@ -1,58 +1,22 @@
-import { Carousel } from '@mantine/carousel';
-import { AppShell, Button, Group, Stack, useMantineColorScheme } from '@mantine/core';
-import Link from 'next/link';
-import React, { useRef, useState } from 'react';
-import { ContactUsForm, MainHeader, SeeRexLoader } from '../../components';
-import BgAnimated from '../../public/BgAnimated.svg';
+import { AppShell, Stack, useMantineColorScheme } from '@mantine/core';
+import React, { useState } from 'react';
+import { ContactUsForm, LandingHeader, MainFooter } from '../../components';
 import style from './_index.module.scss';
-import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures';
 
 function LandingLayout() {
   const [opened, setOpened] = useState(false);
   const { colorScheme } = useMantineColorScheme();
-  const wheelGestures = useRef(WheelGesturesPlugin({ forceWheelAxis: 'y' }));
 
   return (
     <AppShell
-      className={`${style[colorScheme]}`}
-      header={<MainHeader opened={opened} onClick={() => setOpened(!opened)} burger />}
+      className={style[colorScheme]}
+      header={<LandingHeader opened={opened} onClick={() => setOpened(!opened)} burger />}
+      footer={<MainFooter height={56} />}
       padding={0}
-      fixed
     >
-      <Carousel
-        align="start"
-        containScroll="trimSnaps"
-        height="calc(100vh - 80px)"
-        orientation="vertical"
-        plugins={[wheelGestures.current]}
-        speed={4}
-        withControls={false}
-        withIndicators
-      >
-        <Carousel.Slide>
-          <Group className={`${style.page1} ${style[colorScheme]}`} spacing={120} position="center">
-            <Stack spacing={50}>
-              <h1>Monitor your properties.</h1>
-              <h2>Anytime. Anywhere</h2>
-            </Stack>
-            <Link href="/auth/register">
-              <Button type="button" size="lg" radius={10} className={style.button}>
-                Get Started
-              </Button>
-            </Link>
-            <div className={style.backdrop}>
-              <BgAnimated minWidth="100vw" />
-            </div>
-          </Group>
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <Group className={`${style.page1} ${style[colorScheme]}`} spacing={120} position="center">
-            <SeeRexLoader opacity={0.5}/>
-          </Group>
-        </Carousel.Slide>
-      </Carousel>
-
-      <ContactUsForm opened={opened} onClose={() => setOpened(false)} />
+      <Stack align="center">
+        <ContactUsForm opened={opened} onClose={() => setOpened(false)} />
+      </Stack>
     </AppShell>
   );
 }
